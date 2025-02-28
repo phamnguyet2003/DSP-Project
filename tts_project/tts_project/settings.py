5a9b2662497f803bfa2e082fb73c6cbe48934f6a
PhamNguyet192 <hi@gmail.com>
2025-02-28 16:24:59 +0700

    modify settings file

--- tts_project/tts_project/settings.py
+++ tts_project/tts_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,8 @@ SECRET_KEY = "django-insecure-dli9a0(gm!*7vtcd&*!o(i3_kn!&(5v6k$&7nb^j8@)e++a(@7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['chameleonvoice.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,6 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
